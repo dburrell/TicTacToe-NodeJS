1fe87c4f5f860edee1c02df42d8205afad7a7795
dburrell <git@ravensoft.org>
2014-03-12 11:01:08 +0000

    Added turn-taking requirement
    Players are given bool "yourTurn" which is initiated as true for player
    1.
    On each play received, yourTurn is toggled.
    Player can only send play request is yourTurn is true for them.

--- TTT.js
+++ TTT.js
@@ -17,12 +17,14 @@ var app = http.createServer(function (request, response)
       if (players == 0)
       { 
         data = data.replace("[PLAYER]","X"); 
+        data = data.replace("[YOURTURN]","true");
         data = data.replace("[SHOW_WAITING]","solid"); 
       }
       
       if (players == 1)
       { 
         data = data.replace("[PLAYER]","O");
+        data = data.replace("[YOURTURN]","false");
         data = data.replace("[SHOW_WAITING]","none"); 
         io.sockets.emit("player2joined",{});
        }
@@ -49,13 +51,13 @@ var io = require('socket.io').listen(app);
 io.sockets.on('connection', 
   function(socket) 
   {
-    socket.on('message_to_server', function(data) 
+    socket.on('playReq', function(data) 
     {
       if (players === 2)
       {
         var escaped_message = sanitize.escape(data["message"]);
         var playerSubmitted = sanitize.escape(data["player"]);
-        io.sockets.emit("message_to_client",{ message: escaped_message, player: playerSubmitted, debug: false});
+        io.sockets.emit("play",{ message: escaped_message, player: playerSubmitted, debug: false});
       }
       else
       {
